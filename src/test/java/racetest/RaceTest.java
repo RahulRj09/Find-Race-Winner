aea82e3dd579028971653ca2f65655c155133790
RahulRj09 <rahul18@navgurukul.org>
2019-05-29 18:49:58 +0530

    add. test cases for get winner name

--- src/test/java/racetest/RaceTest.java
+++ src/test/java/racetest/RaceTest.java
@@ -1,8 +1,21 @@
 package racetest;
 
 import org.junit.Test;
-import race. *;
-
+import race.*;
+import static org.junit.Assert.*;
 public class RaceTest {
-
+    @Test
+    public void shouldGetWinnerName() {
+        Runner rahul = new Runner("rahul", 99.5);
+        Runner ankita = new Runner("ankita", 9.5);
+        Runner komal = new Runner("koaml", 10.5);
+        Runner pooja = new Runner("pooja", 15.5);
+        Race race = new Race();
+        race.addRunner(rahul);
+        race.addRunner(ankita);
+        race.addRunner(komal);
+        race.addRunner(pooja);
+        String winner = ankita.name;
+        assertEquals(winner,race.getWinner().name);
+    }
 }
